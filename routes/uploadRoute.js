eb204c00fdc6958cb72ea719891b9e384094f4d0
CodeByFaisal26 <faisalpinitod173@gmail.com>
2024-08-17 20:52:46 +0530

    The file is successfully uploaded

--- routes/uploadRoute.js
+++ routes/uploadRoute.js
@@ -1,8 +1,20 @@
 const express = require('express');
-const { uploadFile, uploadMiddleware } = require('../controllers/uploadController');
-
 const router = express.Router();
+const multer = require('multer');
+const { uploadFile } = require('../controllers/uploadController');
 
-router.post('/upload', uploadMiddleware, uploadFile);
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/');
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    },
+});
+
+const upload = multer({ storage: storage });
+
+router.post('/upload', upload.single('file'), uploadFile);
 
 module.exports = router;
+
--- server.js
+++ server.js
@@ -10,7 +10,7 @@ connectDB();
 app.use(express.json());
 
 // Upload route
-app.use('/api/upload', require('./routes/uploadRoute'));
+app.use('/api', require('./routes/uploadRoute'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
